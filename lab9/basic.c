334e4be56155819c8b205e591a6e700a901117d1
luying <91367098+lu1hOAO@users.noreply.github.com>
2024-03-07 14:47:07 +0800

    Create basic.c

---
+++ lab9/basic.c
@@ -0,0 +1,135 @@
+// CONFIG1H
+#pragma config OSC = INTIO67    // Oscillator Selection bits (Internal oscillator block, port function on RA6 and RA7)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+
+// CONFIG2L
+#pragma config PWRT = OFF       // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = SBORDIS  // Brown-out Reset Enable bits (Brown-out Reset enabled in hardware only (SBOREN is disabled))
+#pragma config BORV = 3         // Brown Out Reset Voltage bits (Minimum setting)
+
+// CONFIG2H
+#pragma config WDT = OFF        // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
+
+// CONFIG3H
+#pragma config CCP2MX = PORTC   // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = ON      // PORTB A/D Enable bit (PORTB<4:0> pins are configured as analog input channels on Reset)
+#pragma config LPT1OSC = OFF    // Low-Power Timer1 Oscillator Enable bit (Timer1 configured for higher power operation)
+#pragma config MCLRE = ON       // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+
+// CONFIG4L
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config LVP = OFF        // Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000800-001FFFh) not code-protected)
+#pragma config CP1 = OFF        // Code Protection bit (Block 1 (002000-003FFFh) not code-protected)
+#pragma config CP2 = OFF        // Code Protection bit (Block 2 (004000-005FFFh) not code-protected)
+#pragma config CP3 = OFF        // Code Protection bit (Block 3 (006000-007FFFh) not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000800-001FFFh) not write-protected)
+#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (002000-003FFFh) not write-protected)
+#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (004000-005FFFh) not write-protected)
+#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (006000-007FFFh) not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot block (000000-0007FFh) not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000800-001FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (002000-003FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (004000-005FFFh) not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (006000-007FFFh) not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) not protected from table reads executed in other blocks)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+#include <xc.h>
+#include <pic18f4520.h>
+#define _XTAL_FREQ 1000000
+unsigned char mask=0;
+int high;
+int low;
+int old_value;
+int new_value;
+void __interrupt(high_priority)H_ISR(){
+    
+    //step4
+    high=ADRESH;
+    low = ADRESL;
+    new_value=(high*256)+(low);
+    int differ=new_value-old_value;
+    if(differ<0){
+        differ=0-differ;
+    }
+    if(differ>68){
+        if(mask==30){
+            mask=0;
+        }
+        else {
+            mask+=2;
+        }
+        LATA=mask;
+        __delay_ms(500);
+    }
+    old_value=new_value;
+    PIR1bits.ADIF = 0;
+    
+    
+    //step5 & go back step3
+    
+     __delay_ms(500);
+    ADCON0bits.GO = 1;
+    
+    
+    return;
+}
+
+void main(void) 
+{
+    //configure OSC and port
+    OSCCONbits.IRCF = 0b110; //4MHz
+    LATA=0;
+    old_value=ADRESL;
+    TRISAbits.RA0 = 1;       //analog input port
+    TRISAbits.RA1=0;
+    TRISAbits.RA2=0;
+    TRISAbits.RA3=0;
+    TRISAbits.RA4=0;
+    
+    //step1
+    ADCON1bits.VCFG0 = 0;
+    ADCON1bits.VCFG1 = 0;
+    ADCON1bits.PCFG = 0b1110; //AN0 ?analog input,???? digital
+    ADCON0bits.CHS = 0b0000;  //AN0 ?? analog input
+    ADCON2bits.ADCS = 0b100;  //????100(4Mhz < 5.71Mhz)
+    ADCON2bits.ACQT = 0b001;  //Tad = 4 us acquisition time?2Tad = 8 > 2.4
+    ADCON0bits.ADON = 1;
+    ADCON2bits.ADFM = 1;    //right justified 
+    
+    
+    //step2
+    PIE1bits.ADIE = 1;
+    PIR1bits.ADIF = 0;
+    INTCONbits.PEIE = 1;
+    INTCONbits.GIE = 1;
+
+
+    //step3
+    ADCON0bits.GO = 1;
+    
+    while(1);
+    
+    return;
+}
